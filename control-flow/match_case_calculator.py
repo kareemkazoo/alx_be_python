7011aee9b898caca38497e58a105f7b2482ecd40
KareemKazoo <kimo19950425@gmail.com>
2025-07-09 19:43:11 +0300

    fixed

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,6 +1,6 @@
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-operation = input("Enter operation (+, -, *, /): ")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Choose the operation (+, -, *, /): ")
 
 if operation == "+":
     result = num1 + num2
